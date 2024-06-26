605226a734b97cd593ef271adda0347ee2f21659
yongwoon24 <yongwoon24@naver.com>
2024-06-26 15:19:39 +0900

    0626 R

---
+++ R/exam02.R
@@ -0,0 +1,14 @@
+resno <- c(101, 102, 103, 104, 105)
+rname <- c('ANDREA', 'NY', 'YANIS', 'CLEVETH', 'ASHIYA')
+rage <- c('51', '23', '52', '76', '98')
+
+resident <- data.frame(resno, rname, rage)
+install.packages("readxl")
+library(readxl)
+
+df_exam <- read_excel("C:/RPAWork/workspace/R/csv_exam.csv")
+df_exam2 <- read_excel("C:/RPAWork/workspace/python/_종목_하락순_정렬.xlsx")
+df_exam3 <- read_excel("C:/RPAWork/workspace/R/Data/excel_exam.xlsx")
+print(apply(df_exam3,2,mean))
+
+df_exam3
---
+++ R/exam03.R
@@ -0,0 +1,5 @@
+install.packages("ggplot2")
+library(ggplot2)
+qplot(c("a","b","a","a","b","c"))
+
+qplot(data=mpg,x=hwy)
